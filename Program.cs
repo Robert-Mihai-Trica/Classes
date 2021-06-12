2a8f3883f3562913637deca63520e4aae4bc5f6a
Robbie <Robbie@DESKTOP-UAEN14H>
2021-06-12 13:48:21 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("Tom", 19, 70);
+            s1.SetName("");
+            Console.WriteLine(s1.GetName());
+            s1.SetFinalGrade(110);
+            Console.WriteLine(s1.GetFinalGrade());
+        }
+    }
+}
+ 
